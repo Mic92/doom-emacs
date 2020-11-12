016559dc420adccf732ed73a405121abdcf2d715
Jörg Thalheim <joerg@thalheim.io>
2020-11-12 16:55:51 +0100

    mu4e: hide org-msg behind feature flag

--- modules/email/mu4e/packages.el
+++ modules/email/mu4e/packages.el
@@ -1,4 +1,5 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; email/mu4e/packages.el
 
-(package! org-msg :pin "557d490ecbd80522a42f7b1fb6aaacca504d4512")
+(when (featurep! +org-msg)
+  (package! org-msg :pin "557d490ecbd80522a42f7b1fb6aaacca504d4512"))
